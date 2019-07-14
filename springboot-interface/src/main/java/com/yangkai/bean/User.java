e7caf8564f1fc30e44e9313680746a7d1c554c86
yk0205 <775879106@qq.com>
2019-07-14 19:40:20 +0800

    first commit

---
+++ springboot-interface/src/main/java/com/yangkai/bean/User.java
@@ -0,0 +1,58 @@
+package com.yangkai.bean;
+
+
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+
+    private String id;
+    private String username;
+
+    private String sex;
+    private String address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ springboot-interface/src/main/java/com/yangkai/service/UserService.java
@@ -0,0 +1,15 @@
+package com.yangkai.service;
+
+
+
+import com.yangkai.bean.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUser();
+
+
+
+}
